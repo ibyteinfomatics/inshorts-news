5a4fbbf092329440d7df90a1f2b96b875a796b3f
Deepak <deepak@ibyteinfomatics.in>
2023-11-30 16:14:47 +0530

    edit config

--- config/environment.ts
+++ config/environment.ts
@@ -2,7 +2,8 @@
 export const ENV = {
   PORT: 4500,
   JWT_SECRET_KEY: 'this_is_secret',
-  BASE_URL: 'localhost:4500',
-  DB_URL: 'mongodb://localhost:27017/newsApiDb',
+  BASE_URL: 'https://news-api-up0y.onrender.com',
+  DB_URL:
+    'mongodb+srv://arya:d02Ox3AaoH5wffWk@cluster0.xc0az2u.mongodb.net/newsApiDb',
   API_KEY: '8f7cf579d094498584ffa5e51de2028f',
 };
